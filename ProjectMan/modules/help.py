b3c6b6af5f5778662d2a607fe632fb4cf2adec10
DzLyz <86110711+itsdzl@users.noreply.github.com>
2023-01-24 03:06:50 +0700

    Update help.py

--- ProjectMan/modules/help.py
+++ ProjectMan/modules/help.py
@@ -45,7 +45,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Lunatic0de"
+            this_command += "© Lunatic0de"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
