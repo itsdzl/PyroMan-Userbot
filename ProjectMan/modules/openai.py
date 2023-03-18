cb4ccccd192e430662e4e00a7d6e225747b958f8
DzLyz <86110711+itsdzl@users.noreply.github.com>
2023-03-18 14:05:00 +0700

    Create openai.py

---
+++ ProjectMan/modules/openai.py
@@ -0,0 +1,56 @@
+# if you can read this, this meant you use code from Geez | Ram Project
+# this code is from somewhere else
+# please dont hestitate to steal it
+# because Geez and Ram doesn't care about credit
+# at least we are know as well
+# who Geez and Ram is
+#
+#
+# kopas repo dan hapus credit, ga akan jadikan lu seorang developer
+# ©2023 Geez | Ram Team
+
+import requests
+import openai
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors import MessageNotModified
+from geezlibs.geez import geez
+from geezlibs.geez.helper.what import *
+from Geez import cmds
+from config import OPENAI_API
+from Geez.modules.basic import add_command_help
+
+
+@geez("ask", cmds)
+async def openai(c, m):
+    if len(m.command) == 1:
+        return await m.reply(f"type <code>{cmds}{m.command[0]} [question]</code> Question to use OpenAI")
+    question = m.text.split(" ", maxsplit=1)[1]
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAI_API}",
+    }
+
+    json_data = {
+        "model": "text-davinci-003",
+        "prompt": question,
+        "max_tokens": 200,
+        "temperature": 0,
+    }
+    msg = await m.reply("`Processing..")
+    try:
+        response = (await http.post("https://api.openai.com/v1/completions", headers=headers, json=json_data)).json()
+        await msg.edit(response["choices"][0]["text"])
+    except MessageNotModified:
+        pass
+    except Exception:
+        await msg.edit("**not responding...**")
+
+
+
+add_command_help(
+    "openAI",
+    [
+        [f"{cmds}ask [question]", "to ask questions using the API."],
+    ],
+)
